99bdeeea8e0a10b595300300f8c6abfef78d7efe
Stanko <stanko@bezak.net>
2024-11-16 20:05:26 +0100

    linux pipewire system working

--- linux/audio-control.cpp
+++ linux/audio-control.cpp
@@ -3,43 +3,62 @@
 
 #define PI 3.14159265
 
-void AudioControl::play(float* data) {
+void AudioControl::play(uint8_t* data) {
 
 }
 
-static void registry_event_global(void *data, uint32_t id,
-                uint32_t permissions, const char *type, uint32_t version,
-                const struct spa_dict *props)
+void AudioControl::process(void* data)
 {
-        printf("object: id:%u type:%s/%d\n", id, type, version);
+  std::cout << "Update" << std::endl;
 }
-
-static const struct pw_registry_events registry_events = {
-        PW_VERSION_REGISTRY_EVENTS,
-        .global = registry_event_global,
-};
 
 AudioControl::AudioControl(int argc, char *argv[])
 {
+  const struct spa_pod *params[1];
+  struct spa_pod_builder b = SPA_POD_BUILDER_INIT(buffer, sizeof(buffer));
+ 
   pw_init(&argc, &argv);
   std::cout << pw_get_headers_version() << '\n' << pw_get_library_version() << std::endl;
 
   loop = pw_main_loop_new(NULL);
-  context = pw_context_new(pw_main_loop_get_loop(loop), NULL, 0);
-  core = pw_context_connect(context, NULL, 0);
-  registry = pw_core_get_registry(core, PW_VERSION_REGISTRY, 0);
-  spa_zero(listener);
-  
-  pw_registry_add_listener(registry, listener, &registry_events, NULL);
+  // context = pw_context_new(pw_main_loop_get_loop(loop), NULL, 0);
+  // core = pw_context_connect(context, NULL, 0);
+  output_stream = pw_stream_new_simple(
+    pw_main_loop_get_loop(loop),
+    "sound-auth",
+    pw_properties_new(
+      PW_KEY_MEDIA_TYPE, "Audio",
+      PW_KEY_MEDIA_CATEGORY, "Others",
+      PW_KEY_MEDIA_ROLE, "auth",
+      NULL
+    ),
+    &stream_events,
+    NULL
+  );
+  spa_audio_info_raw info = SPA_AUDIO_INFO_RAW_INIT(
+    .format = SPA_AUDIO_FORMAT_S16,
+    .rate = DEFAULT_RATE,
+    .channels = DEFAULT_CHANNELS
+  );
+  params[0] = spa_format_audio_raw_build(&b, SPA_PARAM_EnumFormat,
+    &info);
+
+  pw_stream_connect(output_stream, 
+    PW_DIRECTION_OUTPUT,
+    PW_ID_ANY,
+    (pw_stream_flags)(PW_STREAM_FLAG_AUTOCONNECT |
+    PW_STREAM_FLAG_MAP_BUFFERS |
+    PW_STREAM_FLAG_RT_PROCESS),
+    params, 1
+  );
 
   pw_main_loop_run(loop);
-
 }
 
 
 AudioControl::~AudioControl() {
-  pw_proxy_destroy((pw_proxy*)registry);
   pw_core_disconnect(core);
   pw_context_destroy(context);
   pw_main_loop_destroy(loop);
+  pw_stream_destroy(output_stream);
 }
--- linux/audio-control.h
+++ linux/audio-control.h
@@ -1,16 +1,30 @@
 #pragma once
-#include <pipewire/pipewire.h>
+extern "C" {
+    #include <spa/param/audio/format-utils.h>
+    #include <pipewire/pipewire.h>
+}
+
+
+#define DEFAULT_CHANNELS 2
+#define DEFAULT_RATE     44100
 
 class AudioControl {
 private: 
     float* data;
     pw_main_loop *loop;
+    pw_stream *output_stream;
     pw_context *context;
     pw_core *core;
-    pw_registry *registry;
     spa_hook *listener;
+    uint8_t buffer[1024];
+    pw_stream_events stream_events
+    {
+        PW_VERSION_STREAM_EVENTS,
+        .process=process
+    };
+    static void process(void*data);
 public:
     AudioControl(int argc, char *argv[]);
-    void play(float* data);
+    void play(uint8_t* data);
     ~AudioControl();
 };
